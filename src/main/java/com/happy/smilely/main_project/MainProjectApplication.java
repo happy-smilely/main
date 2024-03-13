ceaad718095d92c9503f9d50c3a9e91309c0a092
happy-smilely <hjh70344@gmail.com>
2024-03-14 04:09:15 +0900

    SpringBootServletInitializer 수정

--- src/main/java/com/happy/smilely/main_project/MainProjectApplication.java
+++ src/main/java/com/happy/smilely/main_project/MainProjectApplication.java
@@ -2,9 +2,14 @@ package com.happy.smilely.main_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainProjectApplication {
+public class MainProjectApplication extends SpringBootServletInitializer {
+	
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { return application.sources(MainProjectApplication.class); }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainProjectApplication.class, args);
